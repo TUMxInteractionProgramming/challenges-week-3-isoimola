c91556a093f99a050635724e7f8c5c057f0318ee
isoimola <42788897+isoimola@users.noreply.github.com>
2018-09-22 00:52:33 +0200

    week3

---
+++ script.js
@@ -0,0 +1,42 @@
+// JavaScript source code
+
+console.log('app is alive')
+
+
+function switchChannel(yummy) {
+    console.log('Tuning into channel' + 'Yummy');
+    document.getElementById('channelname').innerHTML = 'Yummy';
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    $('#appbarstar').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.pngn.png');
+}
+
+function switchChannel(sevenContinents) {
+    console.log('Tuning into channel' + 'SevenContinents');
+    document.getElementById('channelname').innerHTML = 'SevenContinents';
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    $('#appbarstar').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.pngn.png');
+}
+
+
+function switchChannel(killerApp) {
+    console.log('Tuning into channel' + 'KillerApp');
+    document.getElementById('channelname').innerHTML = 'KillerApp';
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    $('#appbarstar').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.pngn.png');
+}
+
+function switchChannel(firstPersonOnMars) {
+    console.log('Tuning into channel' + 'FirstPersonOnMars');
+    document.getElementById('channelname').innerHTML = 'FirstPersonOnMars';
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    $('#appbarstar').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.pngn.png');
+}
+
+function switchChannel(octoberfest) {
+    console.log('Tuning into channel' + 'Octoberfest');
+    document.getElementById('channelname').innerHTML = 'Octoberfest';
+    document.getElementById('location').innerHTML = 'upgrading.never.helps';
+    $('#appbarstar').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.pngn.png');
+}
+
+function 
